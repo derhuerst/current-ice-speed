b77b6aadb1548f1c6d3bedba520360bd5cc6774e
Jannis R <mail@jannisr.de>
2017-09-07 08:10:01 +0200

    test :white_check_mark:

--- test.js
+++ test.js
@@ -2,7 +2,16 @@
 
 const test = require('tape')
 
-const todo = require('.')
+const speed = require('.')
 
-test('todo', (t) => {
+test('speed', (t) => {
+	t.plan(3)
+
+	speed()
+	.then((speed) => {
+		t.equal(typeof speed, 'number')
+		t.ok(speed >= 0)
+		t.ok(speed <= 500)
+	})
+	.catch(t.ifError)
 })
